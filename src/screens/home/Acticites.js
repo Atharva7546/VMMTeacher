66f6607f74601af61af02e986a5f15107a08bf3b
Atharva <Gadekaratharva02@gmail.com>
2023-11-11 00:33:21 +0530

    Result_Screen

--- src/screens/home/Acticites.js
+++ src/screens/home/Acticites.js
@@ -1,41 +1,155 @@
-import React from 'react';
-import {StyleSheet, Text, SafeAreaView, TouchableOpacity} from 'react-native';
-import {COLORS, ROUTES} from '../../constants';
+import React, { useState } from 'react';
+import { StyleSheet,Image, Text, SafeAreaView, TouchableOpacity, ScrollView, View } from 'react-native';
 
-const Activites = ({navigation}) => {
+const Activites = ({ navigation }) => {
+  const [selectedOptions, setSelectedOptions] = useState(Array(30).fill(null));
+
+  const handleOptionSelect = (questionIndex, optionIndex) => {
+    const updatedOptions = [...selectedOptions];
+    updatedOptions[questionIndex] = optionIndex;
+    setSelectedOptions(updatedOptions);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      // Replace 'YOUR_API_ENDPOINT' with your actual backend API endpoint
+      const apiEndpoint = 'YOUR_API_ENDPOINT';
+      const response = await fetch(apiEndpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ selectedOptions }),
+      });
+
+      // Handle the response from the backend as needed
+      console.log('API Response:', response);
+    } catch (error) {
+      console.error('Error submitting quiz:', error);
+    }
+  };
+
+  const questions = [
+    {
+      question: 'What is your age?',
+      options: ['- Under 18',
+   '- 18-24',
+   '- 25-34',
+   '- 35-44',
+   '- 45 or older'],
+    },
+    {
+      question: 'What is your gender?',
+      options: ['Male','Female','Non-binary','Prefer not to say']
+    },
+    
+    {
+      question: '',
+      options: []
+      },
+    
+    
+    // Add more questions here...
+  ];
+
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: COLORS.bgColor,
-      }}>
-      <Text>Update Result</Text>
-      <TouchableOpacity
-        onPress={() => navigation.goBack()}
-        style={styles.button}
-        activeOpacity={0.8}>
-        <Text style={styles.buttonText}>Go Back</Text>
-      </TouchableOpacity>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+      <View style={styles.header}>
+          <Image
+            source={require('../../assets/logo.png')} // Update with your actual image path
+            style={styles.logo}
+          />
+          <Text style={styles.title}>Quiz Screen</Text>
+        </View>
+        {questions.map((questionData, questionIndex) => (
+          <View key={questionIndex} style={styles.questionContainer}>
+            <Text style={styles.questionText}>{`Question ${questionIndex + 1}: ${questionData.question}`}</Text>
+
+            {questionData.options.map((option, optionIndex) => (
+              <TouchableOpacity
+                key={optionIndex}
+                style={[
+                  styles.optionContainer,
+                  selectedOptions[questionIndex] === optionIndex && styles.selectedOption,
+                ]}
+                onPress={() => handleOptionSelect(questionIndex, optionIndex)}
+              >
+                <Text style={styles.optionText}>{option}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        ))}
+
+        <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
+          <Text style={styles.submitButtonText}>Submit</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
-export default Activites;
-
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor: COLORS.primary,
-    padding: 17,
-    margin: 10,
-    borderRadius: 5,
-    fontSize: 18,
-    width: 180,
+  container: {
+    flex: 1,
+    padding: 16,
   },
-  buttonText: {
-    color: '#fff',
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  logo: {
+    width: 50, // Adjust the width of the logo as needed
+    height: 50, // Adjust the height of the logo as needed
+    marginRight: 10,
+  },
+  title: {
+    fontSize: 24,
     fontWeight: 'bold',
-    textAlign: 'center',
+    marginBottom: 16,
+    color: 'black',
+  },
+  questionContainer: {
+    marginBottom: 20,
+    padding: 16,
+    backgroundColor: '#F4F4F4', // Light gray background
+    borderRadius: 8,
+  },
+  questionText: {
+    fontSize: 18,
+    marginBottom: 8,
+    color: 'black',
+  },
+  optionContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+    padding: 8,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: '#E0E0E0', // Light border color
+  },
+  selectedOption: {
+    backgroundColor: 'lightblue', // Change the color as per your UI theme
+  },
+  optionText: {
+    marginLeft: 8,
+    fontSize: 16,
+    color: 'black',
+  },
+  submitButton: {
+    backgroundColor: 'blue',
+    padding: 16,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  submitButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
+
+export default Activites;
